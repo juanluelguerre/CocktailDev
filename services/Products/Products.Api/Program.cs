cb6c7d67c465c69032ee815d0289144ce8c2692f
Juanlu <juan.luis@nextchapter-ecommerce.com>
2023-09-05 22:58:48 +0200

    Starting to work with OpenTelemetry and ElasticSearch

--- services/Products/Products.Api/Program.cs
+++ services/Products/Products.Api/Program.cs
@@ -1,43 +1,47 @@
-﻿using CocktailDev.Products.Api;
+﻿using System.Text;
+using CocktailDev.Products.Api;
 using CocktailDev.Products.Api.Domain;
 using CocktailDev.Products.Api.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using Serilog;
-using Serilog.Formatting.Elasticsearch;
-using Serilog.Formatting.Json;
-using Serilog.Sinks.Elasticsearch;
-using Serilog.Sinks.File;
 
 var builder = WebApplication.CreateBuilder(args);
 //TODO: Demo (1) Adding Serilog
 builder.Logging.ClearProviders();
 
+
 builder.Host.UseSerilog((context, services, config) =>
 {
+    // Important: Debug Serilog Sinks errors
+    Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+
     config
         .MinimumLevel.Information()
         .Enrich.WithMachineName()
         .WriteTo.Console()
-        .WriteTo.Elasticsearch(
-            new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticSearch:Uri"] ??
-                                                 "http://localhost:9200"))
-            {
-                IndexFormat =
-                    $"{context.Configuration["ApplicationName"]?.ToLower().Replace(".", "-")}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyyMM}",
-                AutoRegisterTemplate = true,
-                DetectElasticsearchVersion = true,
-                ModifyConnectionSettings = x =>
-                    x.BasicAuthentication(context.Configuration["ElasticSearch:User"],
-                        context.Configuration["ElasticSearch:Password"]),
-                FailureCallback = (ex) =>
-                    Console.WriteLine("Unable to submit event " + ex.MessageTemplate),
-                EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
-                                   EmitEventFailureHandling.WriteToFailureSink |
-                                   EmitEventFailureHandling.RaiseCallback,
-                FailureSink = new FileSink($"./fail-{DateTime.UtcNow:yyyyMM}.txt",
-                    new JsonFormatter(), null, null)
-            }).Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+        //.WriteTo.Elasticsearch(
+        //    new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticSearch:Uri"] ??
+        //                                         "http://localhost:9200"))
+        //    {
+        //        IndexFormat =
+        //            $"{context.Configuration["ApplicationName"]?.ToLower().Replace(".", "-")}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyyMM}",
+        //        AutoRegisterTemplate = true,
+        //        ModifyConnectionSettings = c => c
+        //            .ConnectionLimit(-1)
+        //            .BasicAuthentication(context.Configuration["ElasticSearch:User"],
+        //                context.Configuration["ElasticSearch:Password"])
+        //            // TODO: Do not use this at Production environment.
+        //            .ServerCertificateValidationCallback((obj, certificate, chain, errors) => true),
+        //        FailureCallback = (ex) =>
+        //            Console.WriteLine("Unable to submit event " + ex.MessageTemplate),
+        //        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+        //                           EmitEventFailureHandling.WriteToFailureSink |
+        //                           EmitEventFailureHandling.RaiseCallback,
+        //    }).Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
         .ReadFrom.Configuration(context.Configuration);
 });
 
@@ -54,14 +58,45 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddOpenTelemetry()
-//    .WithTracing(tracerProviderBuilder =>
-//        tracerProviderBuilder
-//            .AddSource(DiagnosticsConfig.ActivitySource.Name)
-//            .ConfigureResource(resource => resource
-//                .AddService(DiagnosticsConfig.ServiceName))
-//            .AddAspNetCoreInstrumentation()
-//            .AddConsoleExporter());
+// Ref: https://www.elastic.co/blog/manual-instrumentation-of-net-applications-opentelemetry
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+        tracerProviderBuilder
+            .AddSource(DiagnosticsConfig.ActivitySource.Name)
+            .ConfigureResource(resource => resource
+                .AddService(DiagnosticsConfig.ServiceName))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter()
+            .AddElasticsearchClientInstrumentation(options => { })
+            .AddOtlpExporter(options =>
+            {
+                options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+                options.ExportProcessorType = ExportProcessorType.Simple;
+                options.Endpoint = new Uri("http://localhost:8200");
+                options.Headers = "Authorization=Bearer xxxxxx";
+                //options.HttpClientFactory = () =>
+                //{
+                //    var handler = new HttpClientHandler
+                //    {
+                //        ServerCertificateCustomValidationCallback =
+                //            (sender, cert, chain, sslPolicyErrors) => true
+                //    };
+
+                //    var client = new HttpClient(handler)
+                //    {
+                //        BaseAddress = new Uri("https://localhost:9200")
+                //    };
+
+                //    //const string authenticationString = $"elastic:hx21ixWh2W4OdJWs";
+                //    //var base64EncodedAuthenticationString =
+                //    //    Convert.ToBase64String(Encoding.UTF8.GetBytes(authenticationString));
+                //    //client.DefaultRequestHeaders.Add("Authorization",
+                //    //    "Basic " + base64EncodedAuthenticationString);
+                //    return client;
+                //};
+            })
+    );
 
 
 var app = builder.Build();
