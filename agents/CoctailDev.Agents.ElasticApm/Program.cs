fd48f68491753a95bcb00d7ee6649692c09bd870
Juanlu <jlguerrero@gmail.com>
2023-12-10 12:01:37 +0100

    Observabilit using Elastic and Prometheus+Grafana ready

--- agents/CoctailDev.Agents.ElasticApm/Program.cs
+++ agents/CoctailDev.Agents.ElasticApm/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -22,13 +21,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
 app.UseElasticApm(app.Configuration);
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
--- services/Products/Products.Api/Program.cs
+++ services/Products/Products.Api/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Text;
 using CocktailDev.Products.Api;
 using CocktailDev.Products.Api.Domain;
@@ -13,56 +14,75 @@ using OpenTelemetry.Exporter;
 using Serilog;
 using Google.Protobuf.WellKnownTypes;
 using Microsoft.AspNetCore.Hosting.Server;
+using Serilog.Sinks.OpenTelemetry;
+using Serilog.Core;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
+
 //TODO: Demo (1) Adding Serilog
-builder.Logging.ClearProviders();
 
-builder.Host.UseSerilog((context, services, config) =>
-{
-    // Important: Debug Serilog Sinks errors
-    Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+#region Serilog
 
-    config
-        .MinimumLevel.Information()
-        .Enrich.WithMachineName()
-        .WriteTo.Console()
-        //.WriteTo.Elasticsearch(
-        //    new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticSearch:Uri"] ??
-        //                                         "http://localhost:9200"))
-        //    {
-        //        IndexFormat =
-        //            $"{context.Configuration["ApplicationName"]?.ToLower().Replace(".", "-")}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyyMM}",
-        //        AutoRegisterTemplate = true,
-        //        ModifyConnectionSettings = c => c
-        //            .ConnectionLimit(-1)
-        //            .BasicAuthentication(context.Configuration["ElasticSearch:User"],
-        //                context.Configuration["ElasticSearch:Password"])
-        //            // TODO: Do not use this at Production environment.
-        //            .ServerCertificateValidationCallback((obj, certificate, chain, errors) => true),
-        //        FailureCallback = (ex) =>
-        //            Console.WriteLine("Unable to submit event " + ex.MessageTemplate),
-        //        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
-        //                           EmitEventFailureHandling.WriteToFailureSink |
-        //                           EmitEventFailureHandling.RaiseCallback,
-        //    }).Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
-        .ReadFrom.Configuration(context.Configuration);
+//// builder.Logging.ClearProviders();
+
+////builder.Host.UseSerilog((context, services, config) =>
+////{
+////    // Important: Debug Serilog Sinks errors
+////    Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+////    Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+////    config
+////        .MinimumLevel.Information()
+////        .Enrich.WithMachineName()
+////        .WriteTo.Console()
+////        //.WriteTo.OpenTelemetry(options =>
+////        //{
+////        //    options.Endpoint = "http://localhost:4318/v1/logs";
+////        //    options.Protocol = OtlpProtocol.HttpProtobuf;
+////        //})
+
+////        //endpoint: "http://localhost:4318",
+////        // protocol: OtlpProtocol.HttpProtobuf)
+
+////        //.WriteTo.Elasticsearch(
+////        //    new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticSearch:Uri"] ??
+////        //                                         "http://localhost:9200"))
+////        //    {
+////        //        IndexFormat =
+////        //            $"{context.Configuration["ApplicationName"]?.ToLower().Replace(".", "-")}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyyMM}",
+////        //        AutoRegisterTemplate = true,
+////        //        ModifyConnectionSettings = c => c
+////        //            .ConnectionLimit(-1)
+////        //            .BasicAuthentication(context.Configuration["ElasticSearch:User"],
+////        //                context.Configuration["ElasticSearch:Password"])
+////        //            // TODO: Do not use this at Production environment.
+////        //            .ServerCertificateValidationCallback((obj, certificate, chain, errors) => true),
+////        //        FailureCallback = (ex) =>
+////        //            Console.WriteLine("Unable to submit event " + ex.MessageTemplate),
+////        //        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+////        //                           EmitEventFailureHandling.WriteToFailureSink |
+////        //                           EmitEventFailureHandling.RaiseCallback,
+////        //    }).Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+////        .ReadFrom.Configuration(context.Configuration);
 
 
-    // Ref: https://dev.to/kim-ch/observability-net-opentelemetry-collector-25g1
-    // Only export to OpenTelemetry collector    
-    //config.WriteTo.OpenTelemetry(cfg =>
-    //{
-    //    cfg.Endpoint = $"https://localhost:8200/v1/logs";
-    //    cfg.IncludedData = IncludedData.TraceIdField | IncludedData.SpanIdField;
-    //    cfg.ResourceAttributes = new Dictionary<string, object>
-    //    {
-    //        {"service.name", observabilityOptions.ServiceName},
-    //        {"index", 10},
-    //        {"flag", true},
-    //        {"value", 3.14}
-    //    };
-});
+////    // Ref: https://dev.to/kim-ch/observability-net-opentelemetry-collector-25g1
+////    // Only export to OpenTelemetry collector    
+////    //config.WriteTo.OpenTelemetry(cfg =>
+////    //{
+////    //    cfg.Endpoint = $"https://localhost:8200/v1/logs";
+////    //    cfg.IncludedData = IncludedData.TraceIdField | IncludedData.SpanIdField;
+////    //    cfg.ResourceAttributes = new Dictionary<string, object>
+////    //    {
+////    //        {"service.name", observabilityOptions.ServiceName},
+////    //        {"index", 10},
+////    //        {"flag", true},
+////    //        {"value", 3.14}
+////    //    };
+////});
+
+#endregion
 
 builder.Services.AddMediatR(cfg =>
 {
@@ -72,41 +92,63 @@ builder.Services.AddMediatR(cfg =>
 
 builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Logging.AddOpenTelemetry(logs => logs
+    //.AddConsoleExporter()
+    .AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri("http://localhost:4318/v1/logs");
+        options.Protocol = OtlpExportProtocol.HttpProtobuf;
+    }));
+
 builder.Services.AddOpenTelemetry()
-    .WithTracing(config =>
-        config.SetErrorStatusOnException()
-            .AddSource(DiagnosticsConfig.ActivitySource.Name)
-            .ConfigureResource(resource => resource
-                .AddService(DiagnosticsConfig.ServiceName))
-            .AddAspNetCoreInstrumentation()
-            .AddHttpClientInstrumentation()
-// .AddConsoleExporter()
-//.AddZipkinExporter(o => o.HttpClientFactory = () =>
-//{
-//    var client = new HttpClient();
-//    client.DefaultRequestHeaders.Add("X-MyCustomHeader", "value");
-//    return client;
-//}));
-            .AddOtlpExporter(options =>
-            {
-                //options.Endpoint = new Uri("http://localhost:4317");
-                options.Endpoint =
-                    new Uri("http://opentelemetry-collector.elk.svc:4317");
-                options.Protocol = OtlpExportProtocol.HttpProtobuf;
-                options.Headers = "Authorization=Bearer k9igmeF267v663TW51BlmE2V";
-                // options.ExportProcessorType = ExportProcessorType.Batch;
-                //options.HttpClientFactory = () =>
-                //{
-                //    var client = new HttpClient();
-                //    client.DefaultRequestHeaders.Add("X-MyCustomHeader", "value");
-                //    return client;
-                //};
-            }));
+    .ConfigureResource(resource => resource
+        .AddService(serviceName: builder.Environment.ApplicationName,
+            serviceInstanceId: Environment.MachineName)
+        .AddAttributes(new Dictionary<string, object>
+        {
+            //    ["service.name"] = builder.Environment.ApplicationName.ToLowerInvariant(),
+            //    ["service.node.name"] = Environment.MachineName.ToLowerInvariant(),
+            //    ["service.environment"] = builder.Environment.EnvironmentName.ToLowerInvariant(),
+            ["host.name"] = Environment.MachineName,
+            ["os.description"] = RuntimeInformation.OSDescription,
+            ["deployment.environment"] =
+                builder.Environment.EnvironmentName.ToLowerInvariant(),
+        })
+    )
+    .WithTracing(tracing => tracing.SetErrorStatusOnException()
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        // .AddHangfireInstrumentation()
+        // .AddRedisInstrumentation(connection)
+        // .AddElasticsearchClientInstrumentation()
+        // .AddConsoleExporter()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri("http://localhost:4318/v1/traces");
+            options.Protocol = OtlpExportProtocol.HttpProtobuf;
+        }))
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddProcessInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddPrometheusExporter()
+        // .AddHangfireInstrumentation()
+        // .AddRedisInstrumentation(connection)
+        // .AddElasticsearchClientInstrumentation()
+        .AddMeter("Microsoft.AspNetCore.Hosting")
+        .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri("http://localhost:4318/v1/metrics");
+            options.Protocol = OtlpExportProtocol.HttpProtobuf;
+        }));
+;
 
 
 // Ref: https://www.elastic.co/blog/manual-instrumentation-of-net-applications-opentelemetry
@@ -150,25 +192,6 @@ builder.Services.AddOpenTelemetry()
 //                //};
 //            }));
 
-
-Environment.SetEnvironmentVariable("OTEL_LOG_LEVEL", "1");
-Environment.SetEnvironmentVariable("COREHOST_TRACEFILE", "corehost_verbose_tracing.log");
-
-Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT", "https://localhost:8200");
-Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS",
-    "Authorization=Bearer k9igmeF267v663TW51BlmE2V");
-Environment.SetEnvironmentVariable("OTEL_METRICS_EXPORTER", "otlp");
-Environment.SetEnvironmentVariable("OTEL_LOGS_EXPORTER", "otlp");
-Environment.SetEnvironmentVariable("OTEL_RESOURCE_ATTRIBUTES",
-    "service.name=cocktaildev,service.version=1.0.0,deployment.environment=development");
-
-
-// OTEL_EXPORTER_OTLP_ENDPOINT: // https://localhost:8200 // https://apm-server-apm-http.default.svc:8200 
-// OTEL_EXPORTER_OTLP_HEADERS: "Authorization=Bearer <secret-token>"
-// OTEL_METRICS_EXPORTER: otlp
-// OTEL_LOGS_EXPORTER: otlp
-// OTEL_RESOURCE_ATTRIBUTES: service.name=<app-name>,service.version=<app-version>,deployment.environment=production
-
 // Register a Tracer, so it can be injected into other components (for example, Controllers)
 // builder.Services.AddSingleton(TracerProvider.Default.GetTracer(DiagnosticsConfig.ServiceName));
 
@@ -188,6 +211,8 @@ if (app.Environment.IsDevelopment())
 
 
 // app.UseElasticApm(app.Configuration);
+
+app.MapPrometheusScrapingEndpoint();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
